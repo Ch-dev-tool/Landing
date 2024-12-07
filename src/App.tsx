93f389d12a14db0a75c9334a237097ab485575f6
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-12-07 19:56:35 +0100

    Update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from './components/ui/button'
+import Navbar from './components/utils/Nav.component'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1 className='text-blue-700'>My protofolio </h1>
+      <Button>Click me </Button>
     </>
   )
 }
