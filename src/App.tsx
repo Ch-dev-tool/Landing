6e07f29c0e8e8345498dc84333ca2260ed06042d
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-12-07 20:31:14 +0100

    update app component
    integrat pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import { Button } from './components/ui/button'
-import Navbar from './components/utils/Nav.component'
+import HomePage from './pages/home.page'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <h1 className='text-blue-700'>My protofolio </h1>
-      <Button>Click me </Button>
+      <HomePage />
     </>
   )
 }
