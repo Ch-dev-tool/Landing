5e88ee3287600bdd6b88a2170c0d6e041a230a63
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-12-08 19:43:19 +0100

    update navigation component

--- src/components/utils/Nav.component.tsx
+++ src/components/utils/Nav.component.tsx
@@ -7,6 +7,7 @@ import { Code } from 'lucide-react'
 
 const navItems = [
     { name: 'Home', href: '/' },
+    { name: "About", href: "/#AboutSection" },
     { name: 'Features', href: '/features' },
     { name: 'Pricing', href: '/pricing' },
     { name: 'Contact', href: '/contact' },
