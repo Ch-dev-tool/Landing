d2ed98af00437e84e6e5faf434a8a93aef70a632
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-12-10 19:49:29 +0100

    Update links

--- src/shared/links.ts
+++ src/shared/links.ts
@@ -1,6 +1,7 @@
 const navItems = [
     { name: 'Home', href: '/' },
     { name: "About", href: "/#AboutSection" },
+    { name: "Service", href: "/#ServiceSection"},
     { name: 'Features', href: '/features' },
     { name: 'Pricing', href: '/pricing' },
     { name: 'Contact', href: '/contact' },
